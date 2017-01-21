4053e99fc8b765cd8d224c852858e4de89fb5051
Parthad119 <procks003@gmail.com>
2017-01-21 23:21:34 +0530

    initial commit

---
+++ OceanRacer/Assets/OceanRacer/Script/ObjectFloating.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFloating : Boyancy {
+
+
+	protected override void Start()
+	{
+		base.Start ();
+	}
+	
+
+	protected override void FixedUpdate()
+	{
+		base.FixedUpdate ();
+	}
+}
